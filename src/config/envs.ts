22e6574c1647e1b5b302365401d2f0e797415e9c
jordigd20 <jordigd8@gmail.com>
2024-08-25 14:07:36 +0200

    feat: add stripe payments

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,11 +4,17 @@ import * as Joi from 'joi';
 interface EnvVars {
   PORT: number;
   STRIPE_SECRET: string;
+  STRIPE_SUCCESS_URL: string;
+  STRIPE_CANCEL_URL: string;
+  STRIPE_ENDPOINT_SECRET: string;
 }
 
 const envSchema = Joi.object({
   PORT: Joi.number().required(),
   STRIPE_SECRET: Joi.string().required(),
+  STRIPE_SUCCESS_URL: Joi.string().required(),
+  STRIPE_CANCEL_URL: Joi.string().required(),
+  STRIPE_ENDPOINT_SECRET: Joi.string().required(),
 }).unknown(true);
 
 const { error, value } = envSchema.validate(process.env);
@@ -22,4 +28,7 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   stripeSecret: envVars.STRIPE_SECRET,
+  stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
+  stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
+  stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('PaymentsMS - Main');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  );
 
   await app.listen(envs.port);
   logger.log(`PaymentsMS is running on port: ${envs.port}`);
