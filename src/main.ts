955a4c347387d5ca06536edf2711b2437d95de41
jordigd20 <jordigd8@gmail.com>
2024-08-25 14:06:09 +0200

    feat: add environment config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('PaymentsMS - Main');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(envs.port);
+  logger.log(`PaymentsMS is running on port: ${envs.port}`);
 }
 bootstrap();
